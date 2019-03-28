289e48cc626fa4f66ab19f5da8470d8c0bab14a8
nickoa2001 <oprean.nicolae@gmail.com>
2019-03-28 17:41:03 +0200

    Add files via upload

---
+++ VetTev/Script/ST00_StartApplication.js
@@ -0,0 +1,12 @@
+﻿//USEUNIT CommonReporting
+//USEUNIT CommonApplication
+
+function ST00_StartApplication()
+{
+  Log_InitTestCase("ST00_StartApplication", "Start and Stop Application");
+  Log_CurrentStep("Step 1: Start application");
+  CommonApplication_StartApplication(10000);
+  
+  Log_CurrentStep("Step2: Close the application and store results");
+  CommonReporting.Log_CurrentTestCaseStatusPass();
+}
